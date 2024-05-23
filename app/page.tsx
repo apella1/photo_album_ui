95ee6ddb851a977eabb723ac6a638d05a368c50c
apella1 <johncollons5@gmail.com>
2024-05-23 17:14:59 +0300

    feat: implement dynamic user, album and photo pages

--- app/page.tsx
+++ app/page.tsx
@@ -13,13 +13,13 @@ export default function Home() {
       {!isAuthenticated ? (
         <main className="px-8 md:px-16 xl:px-28 2xl:px-44 py-8 lg:h-screen flex flex-col space-y-6 bg-blue-500 text-white">
           <nav className="flex items-center justify-between">
-            <div className="flex items-center space-x-3">
+            <Link href={"/"} className="flex items-center space-x-3">
               <FaPhotoFilm className="text-5xl" />
               <div className="font-bold text-xl">
                 <h2 className="">Photo Labs</h2>
                 <p className="text-base">Pictures worth 1024 words!</p>
               </div>
-            </div>
+            </Link>
             <div className="flex items-center space-x-4">
               <Link
                 href={"/auth/login"}
