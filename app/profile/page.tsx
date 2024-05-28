6f26b5ba34ab922254c0319aa036e5e56df247a1
apella1 <johncollons5@gmail.com>
2024-05-28 12:47:15 +0300

    refactor: modularize authenticated user navbar menu

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,34 +1,12 @@
 "use client";
 
+import AuthenticatedUserMenu from "@/components/AuthenticatedUserMenu";
 import { useAuthentication } from "@/hooks/useAuthentication";
-import {
-  Avatar,
-  Box,
-  Divider,
-  IconButton,
-  ListItemIcon,
-  Menu,
-  MenuItem,
-  Tooltip,
-} from "@mui/material";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
 import { FaPhotoFilm } from "react-icons/fa6";
-import { IoIosLogOut } from "react-icons/io";
 
 export default function UserProfile() {
-  const { isAuthenticated, user } = useAuthentication();
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const router = useRouter();
+  const { user } = useAuthentication();
 
   return (
     <section className="x-section-padding py-8 flex flex-col space-y-16">
@@ -40,78 +18,7 @@ export default function UserProfile() {
             <p className="text-base">Pictures worth 1024 words!</p>
           </div>
         </Link>
-        {isAuthenticated && (
-          <div className="flex items-center space-x-3 text-lg">
-            <p className="font-medium">Hello, {user?.first_name}</p>
-            <Box
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                textAlign: "center",
-              }}
-            >
-              <Tooltip title="Account settings">
-                <IconButton
-                  onClick={handleClick}
-                  size="small"
-                  aria-controls={open ? "account-menu" : undefined}
-                  aria-haspopup="true"
-                  aria-expanded={open ? "true" : undefined}
-                >
-                  <Avatar sx={{ width: 32, height: 32 }}>
-                    {user?.first_name.charAt(0).toUpperCase()}
-                  </Avatar>
-                </IconButton>
-              </Tooltip>
-            </Box>
-            <Menu
-              anchorEl={anchorEl}
-              id="account-menu"
-              open={open}
-              onClose={handleClose}
-              onClick={handleClose}
-              PaperProps={{
-                elevation: 0,
-                sx: {
-                  overflow: "visible",
-                  filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-                  mt: 1.5,
-                  "& .MuiAvatar-root": {
-                    width: 32,
-                    height: 32,
-                    ml: -0.5,
-                    mr: 1,
-                  },
-                  "&::before": {
-                    content: '""',
-                    display: "block",
-                    position: "absolute",
-                    top: 0,
-                    right: 14,
-                    width: 10,
-                    height: 10,
-                    bgcolor: "background.paper",
-                    transform: "translateY(-50%) rotate(45deg)",
-                    zIndex: 0,
-                  },
-                },
-              }}
-              transformOrigin={{ horizontal: "right", vertical: "top" }}
-              anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-            >
-              <MenuItem onClick={() => router.push("/profile")}>
-                <Avatar /> My account
-              </MenuItem>
-              <Divider />
-              <MenuItem onClick={handleClose}>
-                <ListItemIcon>
-                  <IoIosLogOut />
-                </ListItemIcon>
-                Logout
-              </MenuItem>
-            </Menu>
-          </div>
-        )}
+        <AuthenticatedUserMenu />
       </nav>
       <section className="flex flex-col space-y-6">
         <h2 className="main-title">My Profile</h2>
--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,39 +1,15 @@
 "use client";
 
-import { useAuthentication } from "@/hooks/useAuthentication";
-import {
-  Avatar,
-  Box,
-  Divider,
-  IconButton,
-  ListItemIcon,
-  Menu,
-  MenuItem,
-  Tooltip,
-  Typography,
-} from "@mui/material";
+import AuthenticatedUserMenu from "@/components/AuthenticatedUserMenu";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import React from "react";
 import { FaPhotoFilm } from "react-icons/fa6";
-import { IoIosLogOut } from "react-icons/io";
 
 export default function UsersLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { isAuthenticated, user } = useAuthentication();
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-  const router = useRouter();
-
   return (
     <section className="x-section-padding py-8 flex flex-col space-y-12">
       <nav className="flex items-center justify-between">
@@ -44,78 +20,7 @@ export default function UsersLayout({
             <p className="text-base">Pictures worth 1024 words!</p>
           </div>
         </Link>
-        {isAuthenticated && (
-          <div className="flex items-center space-x-3 text-lg">
-            <p className="font-medium">Hello, {user?.first_name}</p>
-            <Box
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                textAlign: "center",
-              }}
-            >
-              <Tooltip title="Account settings">
-                <IconButton
-                  onClick={handleClick}
-                  size="small"
-                  aria-controls={open ? "account-menu" : undefined}
-                  aria-haspopup="true"
-                  aria-expanded={open ? "true" : undefined}
-                >
-                  <Avatar sx={{ width: 32, height: 32 }}>
-                    {user?.first_name.charAt(0).toUpperCase()}
-                  </Avatar>
-                </IconButton>
-              </Tooltip>
-            </Box>
-            <Menu
-              anchorEl={anchorEl}
-              id="account-menu"
-              open={open}
-              onClose={handleClose}
-              onClick={handleClose}
-              PaperProps={{
-                elevation: 0,
-                sx: {
-                  overflow: "visible",
-                  filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-                  mt: 1.5,
-                  "& .MuiAvatar-root": {
-                    width: 32,
-                    height: 32,
-                    ml: -0.5,
-                    mr: 1,
-                  },
-                  "&::before": {
-                    content: '""',
-                    display: "block",
-                    position: "absolute",
-                    top: 0,
-                    right: 14,
-                    width: 10,
-                    height: 10,
-                    bgcolor: "background.paper",
-                    transform: "translateY(-50%) rotate(45deg)",
-                    zIndex: 0,
-                  },
-                },
-              }}
-              transformOrigin={{ horizontal: "right", vertical: "top" }}
-              anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-            >
-              <MenuItem onClick={() => router.push("/profile")}>
-                <Avatar /> My account
-              </MenuItem>
-              <Divider />
-              <MenuItem onClick={handleClose}>
-                <ListItemIcon>
-                  <IoIosLogOut />
-                </ListItemIcon>
-                Logout
-              </MenuItem>
-            </Menu>
-          </div>
-        )}
+        <AuthenticatedUserMenu />
       </nav>
       <section>{children}</section>
     </section>
