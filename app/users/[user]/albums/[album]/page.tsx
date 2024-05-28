bb6227d0bded5f7a5c97f08e931f07e51a52f308
apella1 <johncollons5@gmail.com>
2024-05-28 18:37:14 +0300

    feat: implement albums dynamic page

--- app/users/[user]/albums/[album]/page.tsx
+++ app/users/[user]/albums/[album]/page.tsx
@@ -1,26 +1,95 @@
+"use client";
+
+import { getAlbumById } from "@/lib/albums";
+import { getAllPhotos } from "@/lib/photos";
+import { DBPhoto } from "@/types/photo";
+import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
 import React from "react";
 
-export default function UserAlbums() {
+export default function UserAlbums({ album }: { album: { id: string } }) {
+  const albumQuery = useQuery({
+    queryKey: ["user"],
+    queryFn: () => getAlbumById(album.id),
+  });
+
+  const photosQuery = useQuery({
+    queryKey: ["photos"],
+    queryFn: getAllPhotos,
+  });
+
   return (
-    <section className="py-8 flex flex-col space-y-6">
-      <div className="flex flex-col space-y-2">
-        <h2 className="sub-title">Album Name</h2>
-        <p>Top Memories</p>
-      </div>
-      <div className="flex flex-col space-y-2">
-        <h2 className="sub-title">Photos</h2>
-        <div className="">
-          <Link href={`/users/${1}/albums/${1}/photos/${1}`}>
-            <div className="flex flex-col space-y-1 p-4 border border-gray-300 rounded-xl w-fit hover:border-blue-400">
-              <p>
-                <span className="font-semibold">Title:</span> Cat
-              </p>
-              <p className="w-[300px] h-[100px]">Preview</p>
+    <>
+      {albumQuery.isLoading ? (
+        <p>Fetching album...</p>
+      ) : albumQuery.isError ? (
+        <p className="text-xl">Album not found.</p>
+      ) : (
+        <section className="py-8 flex flex-col space-y-6">
+          <div className="flex flex-col space-y-2">
+            <h2 className="sub-title">Album Name</h2>
+            <p>{albumQuery.data.title}</p>
+          </div>
+          <div className="flex flex-col space-y-2">
+            <h2 className="sub-title">Albums</h2>
+            <div className="">
+              {photosQuery.isLoading ? (
+                <p>Fetching album&apos;s photos...</p>
+              ) : photosQuery.data.length === 0 ? (
+                <p>{albumQuery.data.title} has no photos yet.</p>
+              ) : (
+                <section>
+                  {photosQuery.data?.filter(
+                    (photo: DBPhoto) => photo?.album_id === albumQuery.data?.id,
+                  ).length == 0 ? (
+                    <p>{albumQuery.data.title} has no photos yet.</p>
+                  ) : (
+                    <section>
+                      {photosQuery.data
+                        ?.filter(
+                          (photo: DBPhoto) =>
+                            photo?.album_id === albumQuery.data?.id,
+                        )
+                        .map((photo: DBPhoto) => (
+                          <Link
+                            href={`/users/${photo.user_id}/albums/${photo.album_id}/photos/${photo.id}`}
+                            key={photo.id}
+                          >
+                            <div className="flex flex-col space-y-1 p-4 border border-gray-300 rounded-xl w-fit hover:border-blue-400">
+                              <p>
+                                <span className="font-semibold">Title:</span>
+                                <p>{photo.title}</p>
+                              </p>
+                              <p>
+                                <span className="font-semibold">
+                                  Number of Photos:
+                                </span>{" "}
+                                {photosQuery.isLoading ? (
+                                  <p>Fetching {`${photo.title}'s`} photos...</p>
+                                ) : photosQuery.data.length === 0 ? (
+                                  <p>0</p>
+                                ) : (
+                                  <p>
+                                    {
+                                      photosQuery.data.filter(
+                                        (photo: DBPhoto) =>
+                                          photo.album_id === photo.id,
+                                      ).length
+                                    }
+                                  </p>
+                                )}
+                              </p>
+                            </div>
+                          </Link>
+                        ))}
+                    </section>
+                  )}
+                </section>
+              )}
             </div>
-          </Link>
-        </div>
-      </div>
-    </section>
+          </div>
+        </section>
+      )}
+    </>
   );
 }
